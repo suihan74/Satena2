a4a14156293d35fed3ded5fba22c0a655685f47d
suihan <suihan742@gmail.com>
2024-06-30 16:23:54 +0900

    fix the bottom padding of FAB on `BookmarkDetailContent` to consider the height of a navigation bar

--- app/src/main/java/com/suihan74/satena2/scene/bookmarks/BookmarkDetailContent.kt
+++ app/src/main/java/com/suihan74/satena2/scene/bookmarks/BookmarkDetailContent.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
@@ -93,10 +94,12 @@ fun BookmarkDetailContent(
 ) {
     Scaffold(
         floatingActionButton = {
+            val navigationBarHeight = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
             item?.let {
                 FloatingActionButton(
                     backgroundColor = CurrentTheme.primary,
                     contentColor = CurrentTheme.onPrimary,
+                    modifier = Modifier.padding(bottom = navigationBarHeight),
                     onClick = { /* todo */ }
                 ) {
                     Image(
